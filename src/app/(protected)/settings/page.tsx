de04a31b1f869c6aff9e04c1ac2e65c2270eedbd
ASHUTOSH KUMAR <ashukumar3436@gmail.com>
2025-05-19 01:54:17 +0530

    file structure fix

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,21 +1,21 @@
-"use client"
-import { auth, signOut } from "@/auth";
-import { LogoutButton } from "@/components/_auth/logout-button";
-import { useRouter } from "next/navigation";
+"use client";
+// import { auth, signOut } from "@/auth";
+import { LogoutButton } from "@/components/auth/logout-button";
+// import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Setting() {
-  const router = useRouter();
+  // const router = useRouter();
   useEffect(() => {
     // if (!session.isAuthenticated) {
     //   router.push("/login");
     // }
   });
-  
+
   return (
     <div className="container">
       {/* <div>{JSON.stringify(session)}</div> */}
-      <LogoutButton /> 
+      <LogoutButton />
     </div>
   );
 }
